ad4f2a8f5a54c50f546101c2863f5356ce653b52
Jonathan Huggins <jhuggins@mit.edu>
2020-02-27 20:53:56 -0500

    getting setup.py ready for PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setup(
     name = 'viabel',
-    version='0.3',
+    version='0.3.0.post1',
     description='Efficient, lightweight, variational inference and approximation bounds',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jonathan H. Huggins',
     author_email='huggins@bu.edu',
     url='https://github.com/jhuggins/viabel/',
     packages=['viabel'],
-    install_requires=['numpy'],
+    install_requires=['numpy>=1.13'],
     extras_require={'vb' : ['scipy', 'autograd', 'tqdm', 'paragami'],
                     'examples' : ['scipy', 'autograd', 'tqdm', 'paragami',
                                   'matplotlib', 'seaborn', 'pandas', 'pystan']},
@@ -17,5 +22,10 @@ setup(
                 'variational inference',
                 'Wasserstein distance',
                 'alpha-divergence'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.5'],
     platforms='ALL',
 )
