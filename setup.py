9eef2cae4a74fd71865a6c7b46a51cb5f97ac7b0
Jonathan Huggins <jhuggins@mit.edu>
2020-10-29 14:40:29 -0400

    only support python versions >=3.5

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+from os import path
 from setuptools import setup
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
 
 setup(
     name = 'viabel',
     version='0.3.0.post1',
     description='Efficient, lightweight, variational inference and approximation bounds',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author='Jonathan H. Huggins',
     author_email='huggins@bu.edu',
     url='https://github.com/jhuggins/viabel/',
@@ -17,15 +21,17 @@ setup(
     extras_require={'vb' : ['scipy', 'autograd', 'tqdm', 'paragami'],
                     'examples' : ['scipy', 'autograd', 'tqdm', 'paragami',
                                   'matplotlib', 'seaborn', 'pandas', 'pystan']},
-    python_requires='>=2.7',
+    python_requires='>=3.5',
     keywords = ['Bayesian inference',
                 'variational inference',
                 'Wasserstein distance',
                 'alpha-divergence'],
-    classifiers=['Development Status :: 4 - Beta',
+    classifiers=['Programming Language :: Python :: 3',
+                 'Natural Language :: English',
                  'License :: OSI Approved :: MIT License',
+                 'Intended Audience :: Science/Research',
+                 'Development Status :: 2 - Pre-Alpha',
                  'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.5'],
+                 'Topic :: Scientific/Engineering :: Mathematics'],
     platforms='ALL',
 )
