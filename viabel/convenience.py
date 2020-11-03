d7421f94605f92fa20fcbbe97dc40e7c68e19ace
Jonathan Huggins <jhuggins@mit.edu>
2020-11-03 11:50:32 -0500

    test chivi too

--- viabel/convenience.py
+++ viabel/convenience.py
@@ -6,7 +6,7 @@ from viabel.objectives import black_box_klvi
 from viabel.optimization import adagrad_optimize
 
 
-def bbvi(dimension, n_iters=10000, n_samples=10, log_density=None, family=None, objective=None, fit=None,  **kwargs):
+def bbvi(dimension, n_iters=10000, n_samples=10, log_density=None, family=None, objective_and_grad=None, fit=None,  **kwargs):
     """Fit a model using black-box variational inference.
 
     Currently the objective is optimized using ``viabel.optimization.adagrad_optimize``.
@@ -41,16 +41,16 @@ def bbvi(dimension, n_iters=10000, n_samples=10, log_density=None, family=None, 
     if log_density is None:
         if fit is None:
             raise ValueError('either log_density or fit must be specified')
-        if objective is not None:
-            raise ValueError('objective can only be specified if log_density is too')
+        if objective_and_grad is not None:
+            raise ValueError('objective_and_grad can only be specified if log_density is too')
         log_density = make_stan_log_density(fit)
     elif fit is not None:
         raise ValueError('log_density and fit cannot both be specified')
     if family is None:
-        if objective is not None:
-            raise ValueError('objective can only be specified if family is too')
+        if objective_and_grad is not None:
+            raise ValueError('objective_and_grad can only be specified if family is too')
         family = mean_field_gaussian_variational_family(dimension)
-    if objective is None:
+    if objective_and_grad is None:
         objective_and_grad = black_box_klvi(family, log_density, n_samples)
     init_param = np.zeros(family.var_param_dim)
     var_param, var_param_history, _, _ = adagrad_optimize(n_iters, objective_and_grad, init_param, **kwargs)
