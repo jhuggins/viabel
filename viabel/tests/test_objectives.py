957ead87ddea3d71fa79278f9f79bcfabb5ceedd
CyrusZhang73 <111306481+CyrusZhang73@users.noreply.github.com>
2023-10-18 15:40:52 -0400

    Update test_objectives.py

--- viabel/tests/test_objectives.py
+++ viabel/tests/test_objectives.py
@@ -1,34 +1,32 @@
 import jax.numpy as jnp
-import numpy as np
+from jax import random
 from jax.scipy.stats import norm
 import pytest
 from viabel.approximations import MFGaussian, MFStudentT
 from viabel.objectives import AlphaDivergence, DISInclusiveKL, ExclusiveKL
 from viabel.optimization import RMSProp
 
-
 def _test_objective(objective_cls, num_mc_samples, **kwargs):
-    np.random.seed(851)
-    # mean = np.random.randn(1,dimension)
-    # stdev = np.exp(np.random.randn(1,dimension))
-    mean = np.array([1., -1.])[np.newaxis, :]
-    stdev = np.array([2., 5.])[np.newaxis, :]
+    rng_key = random.PRNGKey(851)
+
+    mean = jnp.array([1., -1.])[jnp.newaxis, :]
+    stdev = jnp.array([2., 5.])[jnp.newaxis, :]
 
     def log_p(x):
         return jnp.sum(norm.logpdf(x, loc=mean, scale=stdev), axis=1)
 
     approx = MFStudentT(2, 100)
     objective = objective_cls(approx, log_p, num_mc_samples, **kwargs)
-    # large number of MC samples and smaller epsilon and learning rate to ensure accuracy
-    init_param = np.array([0, 0, 1, 1], dtype=np.float32)
+    init_param = jnp.array([0, 0, 1, 1], dtype=jnp.float32)
     opt = RMSProp(0.1)
     opt_results = opt.optimize(400, objective, init_param)
-    # iterate averaging introduces some bias, so use last iterate
     est_mean, est_cov = approx.mean_and_cov(opt_results['opt_param'])
-    est_stdev = np.sqrt(np.diag(est_cov))
+    est_stdev = jnp.sqrt(jnp.diag(est_cov))
     print(est_stdev, stdev)
-    np.testing.assert_almost_equal(mean.squeeze(), est_mean, decimal=1)
-    np.testing.assert_almost_equal(stdev.squeeze(), est_stdev, decimal=1)
+    
+    assert jnp.allclose(mean.squeeze(), est_mean, atol=1e-1)
+    assert jnp.allclose(stdev.squeeze(), est_stdev, atol=1e-1)
+
 
 
 def test_ExclusiveKL():
@@ -82,7 +80,7 @@ def test_DISInclusiveKL():
     dim = 2
     _test_objective(DISInclusiveKL, 100,
                     temper_prior=MFGaussian(dim),
-                    temper_prior_params=np.concatenate([[0] * dim, [1] * dim]),
+                    temper_prior_params=jnp.concatenate([[0] * dim, [1] * dim]),
                     ess_target=50)
 
 
